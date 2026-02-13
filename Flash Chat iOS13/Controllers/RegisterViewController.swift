4326f60e8991317edddf282bfb410915aa96c2c4
Manvitha <manvithachinni444@gmail.com>
2026-02-13 19:51:20 +0530

    added user registration with firebase.

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,30 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        guard let email = emailTextfield.text, !(email.isEmpty) else{
+            emailTextfield.placeholder = "Email is required"
+            print("email is empty Enter something ")
+            return
+        }
+        
+        guard let password = passwordTextfield.text, !(password.isEmpty) else{
+            passwordTextfield.placeholder = "Password is required"
+            print("password is empty Enter something ")
+            return
+        }
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error{
+                print("Some Error while registering \(error)")
+                return
+            }
+            guard let authResult = authResult else{
+                print("Failed to create user")
+                return
+            }
+            print("User Created \(authResult.user)")
+            self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+        }
     }
     
 }
